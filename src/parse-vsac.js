59bf4ca25b88439d68777241f186634c6d6c25ce
Chris Moesel <cmoesel@users.noreply.github.com>
2022-11-29 14:28:44 -0500

    Rename lib to src and format using Prettier (#22)

--- src/parse-vsac.js
+++ src/parse-vsac.js
@@ -1,24 +1,29 @@
 const vsacCS = require('./vsac-code-systems');
 const parseString = require('xml2js').parseString;
-const {Code, ValueSet} = require('cql-execution');
+const { Code, ValueSet } = require('cql-execution');
 
 // Take in a string containing a string of the XML response from a VSAC SVS
 // response and parse it into a vsDB object.  This code makes strong
 // assumptions about the structure of the message.  See code below.
-function parseVSACXML(xmlString, vsDB={}) {
+function parseVSACXML(xmlString, vsDB = {}) {
   if (typeof xmlString === 'undefined' || xmlString == null || xmlString.trim().length == 0) {
     return;
   }
   // Parse the XML string.
   let parsedXML;
-  parseString(xmlString, (err, res) => { parsedXML = res; });
+  parseString(xmlString, (err, res) => {
+    parsedXML = res;
+  });
 
   // Pull out the OID and version for this valueset.
   const vsOID = parsedXML['ns0:RetrieveValueSetResponse']['ns0:ValueSet'][0]['$']['ID'];
   const vsVersion = parsedXML['ns0:RetrieveValueSetResponse']['ns0:ValueSet'][0]['$']['version'];
 
   // Grab the list of codes.
-  const conceptList = parsedXML['ns0:RetrieveValueSetResponse']['ns0:ValueSet'][0]['ns0:ConceptList'][0]['ns0:Concept'];
+  const conceptList =
+    parsedXML['ns0:RetrieveValueSetResponse']['ns0:ValueSet'][0]['ns0:ConceptList'][0][
+      'ns0:Concept'
+    ];
 
   // Loop over the codes and build the JSON.
   const codeList = [];
